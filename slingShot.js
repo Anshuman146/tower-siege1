af8ef6844d4df39730823a2ad66d920d3873e803
Anshuman146 <82568151+Anshuman146@users.noreply.github.com>
2021-05-26 12:11:52 +0530

    Add files via upload

---
+++ slingShot.js
@@ -0,0 +1,32 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        
+        
+        this.pointB = pointB
+        this.polygon = Constraint.create(options);
+        World.add(world, this.polygon);
+    }
+    //attach(body){
+       // this.polygon.bodyA = body;
+   // }
+    
+    fly(){
+        this.polygon.bodyA = null;
+    }
+
+    display(){
+       
+        if(this.polygon.bodyA){
+            var pointA = this.polygon.bodyA.position;
+            var pointB = this.pointB;
+            line (pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+    
+}
